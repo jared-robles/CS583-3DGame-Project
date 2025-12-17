d1897a30c37b38c686f96ab1443d6c49681ef859
justinncruz <jcruz9116@sdsu.edu>
2025-12-16 19:34:17 -0800

    Finished Main Menu and Added Course Selection Screen

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,13 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Panels")]
+    public GameObject HowToPlayPanel;
+
+    void Start()
+    {
+        HowToPlayPanel.SetActive(false);
+    }
+
+    // Main Menu
+
     public void PlayGame() {
-        SceneManager.LoadScene("TestCourse");
+        SceneManager.LoadScene("CourseSelection");
     }
 
     public void QuitGame() {
         Application.Quit();
     }
+
+    // How To Play
+
+    public void OpenHowToPlay()
+    {
+        HowToPlayPanel.SetActive(true);
+    }
+
+    public void CloseHowToPlay()
+    {
+        HowToPlayPanel.SetActive(false);
+    }
+
 }
