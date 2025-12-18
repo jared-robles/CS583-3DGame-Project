7478667baf39a0f68ef85b1ef2a3e1eb0990b532
justinncruz <jcruz9116@sdsu.edu>
2025-12-17 19:07:16 -0800

    Revert "Merge branch 'main' into origin/main-menu"
    This reverts commit 2748624bcae8eda2bbdf28dad236206cede9673f, reversing
    changes made to e20116474050819fc12c1710102caef3d551caf2.

--- Assets/Scripts/Results.cs
+++ Assets/Scripts/Results.cs
@@ -5,30 +5,27 @@ using System.Collections;
 public class Results : MonoBehaviour
 {
     [Header("Inscribed")]
+    // References to the animator and color gradient presets
     public Animator animator;
     public TMP_ColorGradient holeInOneCG;
     public TMP_ColorGradient birdieCG;
     public TMP_ColorGradient parCG;
     public TMP_ColorGradient bogeyCG;
 
-    public GameObject holeInOneFX;
-    public GameObject birdieFX;
-    public GameObject parFX;
-
+    // Store the number of strokes and par count
     private int _strokes;
     private int _par;
-    private bool _inHole;
-    private bool _played;
+    private bool _inHole; // Store ball score status
 
-    private BallController _ball; 
-    private Course _course;
-    private TextMeshProUGUI _txt;
+    private BallController _ball; // Reference to BallController script
+    private Course _course; // Reference to Course script
+    private TextMeshProUGUI _txt; // Reference to the text component
 
     void Start()
     {
-        _ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<BallController>(); 
-        _course = Camera.main.GetComponent<Course>(); 
-        _txt = GetComponent<TextMeshProUGUI>(); 
+        _ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<BallController>(); // BallController script
+        _course = Camera.main.GetComponent<Course>(); // Course script from the main camera
+        _txt = GetComponent<TextMeshProUGUI>(); // This object's TMPro component
     }
 
     void Update()
@@ -52,40 +49,22 @@ public class Results : MonoBehaviour
 
         // Compare number of strokes to the current par
         // Set the respective text and color gradient to its result
-        if (_strokes <= 1)
+        if (_strokes <= 1) // Hole in one
         {
             _txt.colorGradientPreset = holeInOneCG;
             _txt.text = "Hole in One";
-            if (!_played)
-            {
-                Instantiate(holeInOneFX);
-                GameObject.FindGameObjectWithTag("FxTemporaire").transform.position = _ball.gameObject.transform.position;
-                _played = true;
-            }
         }
-        else if (_strokes < _par && _strokes > 1)
+        else if (_strokes < _par && _strokes > 1) // Birdie
         {
             _txt.colorGradientPreset = birdieCG;
             _txt.text = "Birdie";
-            if (!_played)
-            {
-                Instantiate(birdieFX);
-                GameObject.FindGameObjectWithTag("FxTemporaire").transform.position = _ball.gameObject.transform.position;
-                _played = true;
-            }
         }
-        else if (_strokes == _par) 
+        else if (_strokes == _par) // Par
         {
             _txt.colorGradientPreset = parCG;
             _txt.text = "Par";
-            if (!_played) 
-            {
-                Instantiate(parFX);
-                GameObject.FindGameObjectWithTag("FxTemporaire").transform.position = _ball.gameObject.transform.position;
-                _played = true;
-            }
         }
-        else
+        else // Bogey
         {
             _txt.colorGradientPreset = bogeyCG;
             _txt.text = "Bogey";
